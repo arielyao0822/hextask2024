9f67fb51bd84318002e9532c10eb3c42f6b6d44a
ariel.yao <ariel.yao@iisigroup.com>
2024-05-26 21:39:22 +0800

    改html、CSS、還有選單效果

--- scripts.js
+++ scripts.js
@@ -1,3 +1,11 @@
-document.querySelector('.menu-toggle').addEventListener('click', function() {
-  document.querySelector('nav ul').classList.toggle('active');
+document.querySelector('.hamburger-menu .open').addEventListener('click', function() {
+  document.querySelector('.mobile_nav').classList.add('active');
+  document.querySelector('.hamburger-menu .open').style.display = 'none';
+  document.querySelector('.hamburger-menu .close').style.display = 'block';
+});
+
+document.querySelector('.hamburger-menu .close').addEventListener('click', function() {
+  document.querySelector('.mobile_nav').classList.remove('active');
+  document.querySelector('.hamburger-menu .open').style.display = 'block';
+  document.querySelector('.hamburger-menu .close').style.display = 'none';
 });
