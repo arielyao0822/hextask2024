db02e577cef12720b03f993e8a50c9cac8f78a04
Ariel Yao <ariel0822.yao@gmail.com>
2024-05-29 22:48:36 +0800

    再改看看調整選單控制項

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
 document.querySelector('.hamburger-menu .open').addEventListener('click', function() {
-  document.querySelector('.mobile_nav').classList.add('active');
+  document.querySelector('.mobile_menu').classList.add('active');
   document.querySelector('.hamburger-menu .open').style.display = 'none';
   document.querySelector('.hamburger-menu .close').style.display = 'block';
 });
 
 document.querySelector('.hamburger-menu .close').addEventListener('click', function() {
-  document.querySelector('.mobile_nav').classList.remove('active');
+  document.querySelector('.mobile_menu').classList.remove('active');
   document.querySelector('.hamburger-menu .open').style.display = 'block';
   document.querySelector('.hamburger-menu .close').style.display = 'none';
 });
